4d01650a3ec58fd85bfa015658e358038a7dc926
Arthur Meireles <arthurmeireles@MacBook-Pro.local>
2020-07-09 09:16:20 -0300

    Videos dinâmicos, javascript cuidando da modal

--- BasicProject/scripts.js
+++ BasicProject/scripts.js
@@ -1,8 +1,17 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
+const modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".card");
 
 for (let card of cards) {
-    card.addEventListener("click", function(){
-        modalOverlay.classList.add('active');
-    }):
+  card.addEventListener("click", function () {
+    const videoId = card.getAttribute("id");
+    modalOverlay.classList.add("active");
+    modalOverlay.querySelector("iframe").src=`https://www.youtube.com/embed/${videoId}`
+  });
 }
+
+document
+  .querySelector(".close-modal")
+  .addEventListener("click", function () {
+      modalOverlay.classList.remove("active");
+  });
+
