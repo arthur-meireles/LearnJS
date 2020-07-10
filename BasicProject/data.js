f32b45f8c3f30a6b6292fd0f5d00f8a395c4bb39
Arthur Meireles <arthurmeireles@MacBook-Pro.local>
2020-07-10 10:10:13 -0300

    Puxando dados dos cartoes do back-end

---
+++ BasicProject/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "pFKN_YLWYio",
+    title: "Explicando a matrix | Átila Iamarino",
+    duration: "10 min",
+    price: "free",
+  },
+  {
+    id: "7mnf9p4rt5U",
+    title: "O Google sabe de tudo (MESMO) | Átila Iamarino",
+    duration: "10 min",
+    price: "free",
+  },
+  {
+    id: "m5pTiiwnukk",
+    title: "Quão rico você seria se viajasse no tempo | Átila Iamarino",
+    duration: "16 min",
+    price: "free",
+  },
+  {
+    id: "gN-xwvLxC44",
+    title: "O Bug mais famoso dos jogos | Átila Iamarino",
+    duration: "9 min",
+    price: "free",
+  },
+  {
+    id: "F_OrvZKHMSQ",
+    title: "Como fazer um kaiju | Átila Iamarino",
+    duration: "9 min",
+    price: "free",
+  },
+  {
+    id: "SjnIs31kTLo",
+    title: "Roda Crysis? | Átila Iamarino",
+    duration: "12 min",
+    price: "free",
+  }
+];
